75654256f1779ce70007c0571f2c34e79e1dda55
Daksh Kumar <k.daksh29@gmail.com>
2025-04-27 18:58:10 +0530

    setup db

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from routes import user
 
+load_dotenv()
+
 app = Flask(__name__)
+
+mysql_username = os.getenv("MYSQL_USERNAME")
+mysql_password = os.getenv("MYSQL_PASSWORD")
+mysql_db_name = os.getenv("MYSQL_DB_NAME")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{mysql_username}:{mysql_password}@localhost/{mysql_db_name}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 app.register_blueprint(user.user_routes,url_prefix="/user")
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
