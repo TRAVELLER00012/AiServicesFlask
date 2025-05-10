f896af8b79a270c580f0b279e561a740f5a98726
Daksh Kumar <k.daksh29@gmail.com>
2025-05-10 14:22:52 +0530

    remove prod

--- ml_models/classification.py
+++ ml_models/classification.py
@@ -3,45 +3,34 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 import joblib
-import os
-
-IS_PRODUCTION = os.environ.get("RENDER", None) is not None
-
-if IS_PRODUCTION:
-    BASE_MEDIA_PATH = "/tmp"
-else:
-    BASE_MEDIA_PATH = os.path.join(os.getcwd(), "media")
-
-MODEL_DIR = os.path.join(BASE_MEDIA_PATH, "user_models")
-os.makedirs(MODEL_DIR, exist_ok=True)
 
 class LogisticRegressionModel(Model):
     
     @staticmethod
     def fit(X, Y, user_id):
         classifier = LogisticRegression()
-        x = pd.read_csv(X).values
-        y = pd.read_csv(Y).values.ravel()  # flatten y
-        classifier.fit(x, y)
-        joblib.dump(classifier, f"{MODEL_DIR}/{user_id}_LOGISTIC.pkl")
+        x = pd.read_csv(f"media/{X}").iloc[:,:].values
+        y = pd.read_csv(f"media/{Y}").iloc[:,:].values
+        classifier.fit(x,y)
+        joblib.dump(classifier, f"./media/user_models/{user_id}_LOGISTIC.pkl")
     
     @staticmethod
-    def predict(X, user_id):
-        x = pd.read_csv(X).values
-        model = joblib.load(f"{MODEL_DIR}/{user_id}_LOGISTIC.pkl")
+    def predict(X,user_id):
+        x = pd.read_csv(f"media/{X}").iloc[:,:].values
+        model = joblib.load(f"./media/user_models/{user_id}_LOGISTIC.pkl")
         return model.predict(x)
 
 class DecissionTreeC(Model):
     @staticmethod
-    def fit(X, Y, user_id):
+    def fit(X, Y,user_id):
         classifier = DecisionTreeClassifier()
-        x = pd.read_csv(X).values
-        y = pd.read_csv(Y).values.ravel()
-        classifier.fit(x, y)
-        joblib.dump(classifier, f"{MODEL_DIR}/{user_id}_DTC.pkl")
+        x = pd.read_csv(f"media/{X}").iloc[:,:].values
+        y = pd.read_csv(f"media/{Y}").iloc[:,:].values
+        classifier.fit(x,y)
+        joblib.dump(classifier, f"./media/user_models/{user_id}_DTC.pkl")
     
     @staticmethod
-    def predict(X, user_id):
-        x = pd.read_csv(X).values
-        model = joblib.load(f"{MODEL_DIR}/{user_id}_DTC.pkl")
+    def predict(X,user_id):
+        x = pd.read_csv(f"media/{X}").iloc[:,:].values
+        model = joblib.load(f"./media/user_models/{user_id}_DTC.pkl")
         return model.predict(x)
--- ml_models/regression.py
+++ ml_models/regression.py
@@ -1,64 +1,58 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
 from .model import Model
 import joblib
-import os
 
-IS_PRODUCTION = os.environ.get("RENDER", None) is not None
-
-if IS_PRODUCTION:
-    BASE_MEDIA_PATH = "/tmp"
-else:
-    BASE_MEDIA_PATH = os.path.join(os.getcwd(), "media")
-
-MODEL_DIR = os.path.join(BASE_MEDIA_PATH, "user_models")
-os.makedirs(MODEL_DIR, exist_ok=True)
 class Linear(Model):
     @staticmethod
-    def fit(X, Y, user_id):
+    def fit(X,Y, user_id):
         regressor = LinearRegression()
-        x = pd.read_csv(X).values
-        y = pd.read_csv(Y).values.ravel()
-        regressor.fit(x, y)
-        joblib.dump(regressor, f"{MODEL_DIR}/{user_id}_linear.pkl")
+        x = pd.read_csv(f"media/{X}").values
+        y = pd.read_csv(f"media/{Y}").values
+        regressor.fit(x,y)
+        joblib.dump(regressor, f"media/user_models/{user_id}_linear.pkl")
 
     @staticmethod
     def predict(X, user_id):
-        x = pd.read_csv(X).values
-        model = joblib.load(f"{MODEL_DIR}/{user_id}_linear.pkl")
-        return model.predict(x)
+        x = pd.read_csv(f"media/{X}").values
+        model = joblib.load(f"media/user_models/{user_id}_linear.pkl")
+        predictions = model.predict(x)
+        return predictions
 
 
 class SVR_Model(Model):
-    def __init__(self, kernel="rbf", gamma="auto", degree=3, C=1.0, e=0.1):
-        self.regressor = SVR(kernel=kernel, gamma=gamma, degree=degree, C=C, epsilon=e)
+    def __init__(self, kernel, gamma = "auto" ,degree = 3, C = 1.0, e = 0.1):
+        self.regressor = SVR(kernel=kernel,gamma=gamma,degree=degree,C=C, epsilon=e)
 
-    def fit(self, X, Y, user_id):
-        x = pd.read_csv(X).values
-        y = pd.read_csv(Y).values.ravel()
-        self.regressor.fit(x, y)
-        joblib.dump(self.regressor, f"{MODEL_DIR}/{user_id}_SVR.pkl")
 
-    @staticmethod
-    def predict(X, user_id):
-        x = pd.read_csv(X).values
-        model = joblib.load(f"{MODEL_DIR}/{user_id}_SVR.pkl")
-        return model.predict(x).reshape(-1, 1)
-
+    def fit(self,X,Y,user_id):
+        x = pd.read_csv(f"media/{X}").iloc[:,:].values
+        y = pd.read_csv(f"media/{Y}").iloc[:,:].values
+        self.regressor.fit(x,y)
+        joblib.dump(self.regressor,f"media/user_models/{user_id}_SVR.pkl")
+        
+    @staticmethod    
+    def predict(X,user_id):
+        x = pd.read_csv(f"media/{X}").iloc[:,:].values
+        model = joblib.load(f"media/user_models/{user_id}_SVR.pkl")
+        predictions = model.predict(x).reshape(-1,1)
+        return predictions
+    
 
 class DecisionTreeRModel(Model):
     @staticmethod
-    def fit(X, Y, user_id):
+    def fit(X,Y,user_id):
         regressor = DecisionTreeRegressor()
-        x = pd.read_csv(X).values
-        y = pd.read_csv(Y).values.ravel()
-        regressor.fit(x, y)
-        joblib.dump(regressor, f"{MODEL_DIR}/{user_id}_DTR.pkl")
-
+        x = pd.read_csv(f"media/{X}").iloc[:,:].values
+        y = pd.read_csv(f"media/{Y}").iloc[:,:].values
+        regressor.fit(x,y)
+        joblib.dump(regressor,f"media/user_models/{user_id}_DTR.pkl")
+        
     @staticmethod
-    def predict(X, user_id):
-        x = pd.read_csv(X).values
-        model = joblib.load(f"{MODEL_DIR}/{user_id}_DTR.pkl")
+    def predict(X,user_id):
+        x = pd.read_csv(f"media/{X}").iloc[:,:].values
+        model = joblib.load(f"media/user_models/{user_id}_DTR.pkl")
         return model.predict(x)
